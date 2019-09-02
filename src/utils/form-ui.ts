914024fbb0d42df3f9ea9e5b1f3cea728d3b9dc9
Jairo  Campos <jairocampos@Jairos-MacBook-Pro.local>
2019-09-02 17:47:50 -0600

    merge data from pod to layout

--- src/utils/form-ui.ts
+++ src/utils/form-ui.ts
@@ -1,4 +1,5 @@
 import data from '@solid/query-ldflex'
+import auth from 'solid-auth-client'
 import { CONTEXT } from '../constants'
 
 /**
@@ -63,27 +64,59 @@ function getPredicateName(predicate: string): any {
   return null
 }
 
+async function getPropertyValue(field: string, property: string) {
+  let propertyProxy: any = await data[field][property]
+
+  return propertyProxy && propertyProxy.value
+}
+
 /**
  * Convert turtle to Json-ld object
  * @param document
  * @param partsPath
  */
-async function turtleToFormUi(document: any, partsPath: any) {
+async function turtleToFormUi(document: any, userData: string, parentValue?: any) {
   let fields: any = {}
   const doc = await document
+  const partsPath = 'http://www.w3.org/ns/ui#parts'
   const parts: any = await loopList(doc[partsPath])
 
   for await (const field of parts) {
     const subjectKey: string = getPredicateName(field)
     const subjectPrefix = `subject:${subjectKey}`
+    let fieldValue = {}
 
     for await (const property of data[field].properties) {
-      let propertyProxy: any = await data[field][property]
       let partsFields: any = {}
-      let propertyValue: string = propertyProxy && propertyProxy.value
+      let propertyValue: string = await getPropertyValue(field, property)
 
-      if (property.includes('parts')) {
-        partsFields = await turtleToFormUi(data[field], property)
+      /**
+       * If property exist into the subject we added it into the json-ld object
+       */
+      if (property.includes('parts') && propertyValue) {
+        let layoutKey = await getPropertyValue(field, 'http://www.w3.org/ns/ui#property')
+        let userFieldValue = null
+
+        if (layoutKey && userData) {
+          userFieldValue = await data[userData][layoutKey]
+        }
+
+        partsFields = await turtleToFormUi(
+          data[field],
+          (userFieldValue && userFieldValue.value) || userData
+        )
+      }
+
+      if (property.includes('property') && propertyValue) {
+        if (userData || parentValue) {
+          const value = await data[userData || parentValue][propertyValue]
+          if (value && value.value) {
+            const { value: currenValue } = value
+            fieldValue = { value: currenValue, oldValue: currenValue }
+          } else {
+            fieldValue = { value: '', oldValue: '' }
+          }
+        }
       }
 
       const propertyKey: string = getPredicateName(property)
@@ -91,7 +124,8 @@ async function turtleToFormUi(document: any, partsPath: any) {
       let newField = {
         [subjectPrefix]: {
           ...fields[subjectPrefix],
-          [propertyKey]: Object.keys(partsFields).length === 0 ? propertyValue : partsFields
+          [propertyKey]: Object.keys(partsFields).length === 0 ? propertyValue : partsFields,
+          ...fieldValue
         }
       }
 
@@ -128,16 +162,26 @@ function subjectPrefix(document: any) {
   return `${document}#`
 }
 
+async function existDocument(document: string) {
+  const result = await auth.fetch(document, { method: 'GET' })
+
+  return result.status !== 404
+}
+
 /**
  * Convert turtle to formModel(JSON-LD)
  * @param documentUri
  * @param partsPath
  */
-export async function convertFormModel(
-  documentUri: any,
-  partsPath: any = 'http://www.w3.org/ns/ui#parts'
-) {
-  const model = await turtleToFormUi(data[documentUri], partsPath)
+export async function convertFormModel(documentUri: any, documentPod: any) {
+  const existDocumentPod = await existDocument(documentPod)
+  let userData = null
+
+  /* if (existDocumentPod) {
+    userData = await data[documentPod]
+  } */
+
+  const model = await turtleToFormUi(data[documentUri], documentPod)
 
   return {
     '@context': {
