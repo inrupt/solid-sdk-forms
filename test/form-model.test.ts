5b8f50ada00f68dc94d01237b61e8a6e9f708313
pmcb55 <patm@inrupt.com>
2020-06-09 17:10:28 +0100

    Just getting tests to pass.

--- test/form-model.test.ts
+++ test/form-model.test.ts
@@ -4,7 +4,7 @@ const url = 'https://shexshapes.inrupt.net/public/contact.shex'
 
 describe('FormModel', () => {
   it('should return file ext from url path', () => {
-    const instance = new FormModel(url)
+    const instance = new FormModel(url, 'Unused Param!')
     const fileExt = instance.schemaType(url)
 
     expect(fileExt).toEqual('shex')
